9ddc67a4b9b7d689d1b95f2262c7b64da13ce1a2
BT <tabengwa@gmail.com>
2017-07-25 20:33:57 -0500

    rectified linear activation function

---
+++ relu_function.py
@@ -0,0 +1,7 @@
+def relu(input):
+	"""Define your relu activation function here"""
+	# Calculate the value for the output of the relu function: output
+	output = max(0, input)
+
+	# Return the value you just Calculated
+	return(output)
